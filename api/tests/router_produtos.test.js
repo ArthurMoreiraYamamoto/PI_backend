f2282e724cce1530012baf3e93174233bd1faf9f
pablo r. <pablo.silva2722@gmail.com>
2024-06-16 17:27:28 -0300

    teste de rota PUT funcionando #22

--- api/tests/router_produtos.test.js
+++ api/tests/router_produtos.test.js
@@ -1,7 +1,5 @@
 const supertest = require('supertest');
-
 const app = require('../app');
-
 const request = supertest(app);
 
 describe('API', () => {
@@ -23,19 +21,38 @@ describe('API', () => {
     expect(response.status).toBe(200);
     expect(response.type).toBe('application/json');
     if (response.body.length > 0) {
-        id = response.body[0]._id.toString();
+      id = response.body[0]._id.toString();
     }
-});
+  });
 
-test('retorna 200 e um JSON no GET /produtos/id', async () => {
+  test('retorna 200 e um JSON no GET /produtos/id', async () => {
     const response = await request.get(`/produtos/${id}`);
     expect(response.status).toBe(200);
     expect(response.type).toBe('application/json');
   });
 
   test('retorna 404 e um JSON no GET /produtos/id', async () => {
-    const response = await request.get('/produtos/664901b4f1aa5565cb12f409');
+    const response = await request.get('/produtos/666f3837c1871e678d6ff86b');
     expect(response.status).toBe(404);
     expect(response.type).toBe('application/json');
   });
+
+  test('retorna 200 e um JSON no PUT /produtos/id', async () => {
+    const response = await request.put(`/produtos/${id}`).send({ nome: "iscaViva", preco: 15.0 });
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('application/json');
+  });
+
+  test('retorna 404 e um JSON no PUT /produtos/id', async () => {
+    const response = await request.put('/produtos/666f380885ea5aaf047f8f39');
+    expect(response.status).toBe(404);
+    expect(response.type).toBe('application/json');
+  });
+
+  test('retorna 422 e um JSON no PUT /produtos', async () => {
+    const response = await request.put(`/produtos/${id}`).send({});
+    expect(response.status).toBe(422);
+    expect(response.type).toBe("application/json");
+  });
+
 });
