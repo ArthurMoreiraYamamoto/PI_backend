866291f35dd129a3c9e72aa3938c4127751aaa5d
pablo r. <pablo.silva2722@gmail.com>
2024-06-16 15:13:32 -0300

    validador de dados POST #25 #30

--- api/controllers/controller_produtos.js
+++ api/controllers/controller_produtos.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 const Produto = require('../models/model_produtos');
 
-async function novoProduto(req, res) {
+const validarDados = async (req, res, next) => {
+    const produto = new Produto(req.body);
+    try {
+        await produto.validate();
+        next();
+    } catch (error) {
+        res.status(422).json({ msg: 'Dados invalidos' });
+    };
+};
+
+const novoProduto = async (req, res) => {
     const produto = await Produto.create(req.body);
     res.status(201).json(produto);
-}
-module.exports = { validarDados, novoProduto }
+};
+
+module.exports = { validarDados, novoProduto };
--- api/routes/router_produtos.js
+++ api/routes/router_produtos.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const controllerProdutos = require("../controllers/controller_produtos");
 
 
-router.post("/", controllerProdutos.validarDados, controllerProdutos.criar);
+router.post('/', controllerProdutos.validarDados, controllerProdutos.novoProduto);
 
 module.exports = router;
