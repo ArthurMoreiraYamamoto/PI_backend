49f4968f6a45deea0cea0df276dd2053f36c1316
unknown <2214290113@academico.iesb.br>
2024-06-18 20:50:51 -0300

    documentação completa #50

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const routerApiDocs = require('./routes/router_apidocs');
 const routerProdutos = require('./routes/router_produtos');
 
 const app = express();
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use('/api-docs', routerApiDocs);
 app.use('/produtos', routerProdutos);
+
 
 module.exports = app;
